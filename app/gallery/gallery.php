cac5511bee4bb744df4ece802d915b0d32051a6c
olmos <olmosescolatreball@gmail.com>
2021-10-31 19:27:45 +0100

    quitando echo's en gallery . working ok

--- app/gallery/gallery.php
+++ app/gallery/gallery.php
@@ -5,20 +5,20 @@ include('./Class/GalleryClass.php');
 $filename = $_GET['ruta'];
 $gallery = new Gallery($filename);
 $gallery->loadGallery();
-
-echo '<div class="container">';
-echo '<div class="card-group flex-gallery">';
-foreach ($gallery->__get("_gallery") as $pic) {
-    echo '<div class="col-sm-12 col-md-4">';
-    echo '<div class="card-column ">';
-    echo '<div class="custom-column-content">';
-    echo '<img   src="' . $pic->__get('filename') . '" style="object-fit:fill;width:90%;height:300;border: solid 1px #CCC" alt="">';
-    echo '<p align="center">' . $pic->__get('title') . '</p>';
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-}
-echo '</div>';
-echo '</div>';
 ?>
+<div class="container">
+<div class="card-group flex-gallery">
+<?php foreach ($gallery->__get("_gallery") as $pic) { ?>
+    <div class="col-sm-12 col-md-4">
+    <div class="card-column ">
+    <div class="custom-column-content">
+        <!-- todo todavia se puede reducir unm poquito mas el tag de php -->
+    <?php echo '<img   src="' . $pic->__get('filename') . '" style="object-fit:fill;width:90%;height:300;border: solid 1px #CCC" alt="">'?>
+     <?php echo '<p align="center">' . $pic->__get('title') . '</p>'?>
+    </div>
+    </div>
+    </div>
+<?php } ?>
+</div>
+</div>
 <?php include_once('_footer.php') ?>
