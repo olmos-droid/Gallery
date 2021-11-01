c69ef4989823fea0ed99eeef84cf3f044ae87a80
olmos <olmosescolatreball@gmail.com>
2021-11-01 01:34:03 +0100

    ok echo

--- app/gallery/gallery.php
+++ app/gallery/gallery.php
@@ -7,18 +7,18 @@ $gallery = new Gallery($filename);
 $gallery->loadGallery();
 ?>
 <div class="container">
-<div class="card-group flex-gallery">
-<?php foreach ($gallery->__get("_gallery") as $pic) { ?>
-    <div class="col-sm-12 col-md-4">
-    <div class="card-column ">
-    <div class="custom-column-content">
-        <!-- todo todavia se puede reducir unm poquito mas el tag de php -->
-    <?php echo '<img   src="' . $pic->__get('filename') . '" style="object-fit:fill;width:90%;height:300;border: solid 1px #CCC" alt="">'?>
-     <?php echo '<p align="center">' . $pic->__get('title') . '</p>'?>
+    <div class="card-group flex-gallery">
+        <?php foreach ($gallery->__get("_gallery") as $pic) { ?>
+            <div class="col-sm-12 col-md-4">
+                <div class="card-column ">
+                    <div class="custom-column-content">
+                        <!-- todo todavia se puede reducir unm poquito mas el tag de php -->
+                        <img src="<?php echo $pic->__get('filename') ?>" style="object-fit:fill;width:90%;height:300;border: solid 1px #CCC" alt="">
+                        <p align="center"><?php echo $pic->__get('title')?></p>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
     </div>
-    </div>
-    </div>
-<?php } ?>
-</div>
 </div>
 <?php include_once('_footer.php') ?>
